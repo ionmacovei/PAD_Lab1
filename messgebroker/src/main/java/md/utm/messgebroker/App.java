92b2ca0429afda96b0b9357c0bbf7de9e74baa5d
ionmacovei93@gmail.com <ionmacovei93@gmail.com>
2016-09-15 17:36:00 +0300

    first commmit in eclipse

---
+++ messgebroker/src/main/java/md/utm/messgebroker/App.java
@@ -0,0 +1,13 @@
+package md.utm.messgebroker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
